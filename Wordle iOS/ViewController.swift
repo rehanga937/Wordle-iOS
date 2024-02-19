25a810a28e21fb237348559b0b9dad011e8ac2b3
Rehanga Gamage <rehanga@live.com>
2024-02-19 11:37:30 +0530

    Same as before, forgot to stage some files.

--- Wordle iOS/ViewController.swift
+++ Wordle iOS/ViewController.swift
@@ -14,6 +14,30 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet weak var Label_1: UILabel!
+    @IBOutlet weak var Label_2: UILabel!
+    @IBOutlet weak var Label_3: UILabel!
+    @IBOutlet weak var Label_4: UILabel!
+    @IBOutlet weak var Label_5: UILabel!
+    @IBOutlet weak var Label_6: UILabel!
+    @IBOutlet weak var Label_7: UILabel!
+    @IBOutlet weak var Label_8: UILabel!
+    @IBOutlet weak var Label_9: UILabel!
+    @IBOutlet weak var Label_10: UILabel!
+    @IBOutlet weak var Label_11: UILabel!
+    @IBOutlet weak var Label_12: UILabel!
+    @IBOutlet weak var Label_13: UILabel!
+    @IBOutlet weak var Label_14: UILabel!
+    @IBOutlet weak var Label_15: UILabel!
+    @IBOutlet weak var Label_16: UILabel!
+    @IBOutlet weak var Label_17: UILabel!
+    @IBOutlet weak var Label_18: UILabel!
+    @IBOutlet weak var Label_19: UILabel!
+    @IBOutlet weak var Label_20: UILabel!
+    @IBOutlet weak var Label_21: UILabel!
+    @IBOutlet weak var Label_22: UILabel!
+    @IBOutlet weak var Label_23: UILabel!
+    @IBOutlet weak var Label_24: UILabel!
+    @IBOutlet weak var Label_25: UILabel!
 }
 
